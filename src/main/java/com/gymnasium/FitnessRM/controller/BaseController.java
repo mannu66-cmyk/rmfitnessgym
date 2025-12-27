fe5025c4988c0f36d40d7b964db1ae943027c200
Manoj Singh Rawat <mnjsinghrawat6@gmail.com>
2025-12-27 15:53:33 +0530

    check deployment

---
+++ src/main/java/com/gymnasium/FitnessRM/controller/BaseController.java
@@ -0,0 +1,16 @@
+package com.gymnasium.FitnessRM.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gymnasium.FitnessRM.utils.JwtUtil;
+
+@RestController
+public class BaseController {
+
+    @GetMapping 
+    public String check() {
+
+        return "running......";
+    }
+}
