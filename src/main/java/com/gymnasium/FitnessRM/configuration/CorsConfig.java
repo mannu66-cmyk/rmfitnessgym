7ed2157221653ccdcd456fcbc8394dfc1804600b
Manoj Singh Rawat <mnjsinghrawat6@gmail.com>
2025-12-29 19:24:45 +0530

    Update CorsConfig.java

--- src/main/java/com/gymnasium/FitnessRM/configuration/CorsConfig.java
+++ src/main/java/com/gymnasium/FitnessRM/configuration/CorsConfig.java
@@ -16,8 +16,8 @@ public class CorsConfig {
 
 		CorsConfiguration config = new CorsConfiguration();
 
-		config.setAllowedOrigins(List.of("http://localhost:4200", "https://rmfitnessgym.online/",
-				"https://d1dd3v3n3zjtgf.cloudfront.net/", "http://3.7.104.186:8080",
+		config.setAllowedOrigins(List.of("http://localhost:4200", "https://rmfitnessgym.online",
+				"https://d1dd3v3n3zjtgf.cloudfront.net", "http://3.7.104.186:8080",
 				"http://info-angular-app-2025.s3-website.ap-south-1.amazonaws.com"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
