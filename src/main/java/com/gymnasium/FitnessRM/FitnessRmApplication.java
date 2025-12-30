fe2ee9d1a7ae3cd3dc213a17435e8329361e2b0c
Manoj Singh Rawat <mnjsinghrawat6@gmail.com>
2025-12-30 12:50:29 +0530

    Update FitnessRmApplication.java

--- src/main/java/com/gymnasium/FitnessRM/FitnessRmApplication.java
+++ src/main/java/com/gymnasium/FitnessRM/FitnessRmApplication.java
@@ -2,7 +2,7 @@ package com.gymnasium.FitnessRM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
